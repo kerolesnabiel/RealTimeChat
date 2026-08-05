f08b219cde401161afa9006e96d153c86153904f
kerolesnabiel <keronabilzaki@gmail.com>
2026-08-05 16:06:24 +0300

    Add user search feature

---
+++ RealTimeChatAPI/Features/Users/Search.cs
@@ -0,0 +1,55 @@
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealTimeChatAPI.Common.Endpoints;
+using RealTimeChatAPI.Common.Messaging;
+using RealTimeChatAPI.Database;
+
+namespace RealTimeChatAPI.Features.Users;
+
+internal static class Search
+{
+    public sealed record UserDto(Guid Id, string Username, string Name, string? Image);
+    public sealed record Query(string SearchName) : IQuery<IEnumerable<UserDto>>;
+
+    internal sealed class Handler(ApplicationDbContext dbContext) : IQueryHandler<Query, IEnumerable<UserDto>>
+    {
+        public async Task<IEnumerable<UserDto>> Handle(Query query, CancellationToken cancellationToken)
+        {
+            var search = query.SearchName.Trim();
+
+            if (search.Length < 3)
+                return [];
+
+            var users = await dbContext.Users
+                .Where(x => x.Username.StartsWith(search) ||
+                    x.Name.Contains(search))
+                .OrderByDescending(x => x.Username.StartsWith(search))
+                .ThenBy(x => x.Username)
+                .ThenBy(x => x.Name)
+                .Take(5)
+                .Select(x => x.Adapt<UserDto>())
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return users;
+        }
+    }
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/users", async (
+                [FromQuery] string SearchName,
+                IQueryHandler<Query, IEnumerable<UserDto>> Handler,
+                CancellationToken cancellationToken) =>
+            {
+                var users = await Handler.Handle(new(SearchName), cancellationToken);
+                return Results.Ok(users);
+            })
+            .RequireAuthorization()
+            .WithTags(Tags.Users);
+        }
+    }
+}
