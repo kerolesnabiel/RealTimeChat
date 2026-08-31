3280112b90c956f5b36aa2200234c7ba90f1fc5f
kerolesnabiel <keronabilzaki@gmail.com>
2026-08-31 21:01:43 +0300

    Initial setup

---
+++ RealTimeChatWebApp/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-400">
+      <h1 className="text-3xl font-bold">Welcome to the Real-Time Chat</h1>
+    </div>
+  );
+}
+
+export default Home;
